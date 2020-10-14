943b017e9f95bef24ce11eb55170dafb941fa5d2
wss-git <wssgryx@163.com>
2020-10-14 17:26:22 +0800

    监控前端源码

---
+++ src/utils/metrics/metrics/src/mock.js
@@ -0,0 +1,390 @@
+export default {
+  "FunctionTotalInvocations": [
+    {
+      "timestamp": 1602209340000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "test",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602209460000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210120000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602210300000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 2
+    },
+    {
+      "timestamp": 1602210360000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602210780000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210960000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602212100000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602212400000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "test",
+      "functionName": "fun-nas-function",
+      "Value": 0
+    }
+  ],
+  "FunctionMeteringInvocations": [{
+    "timestamp": 1602209340000,
+    "userId": "1899690531354629", "region": "cn-shenzhen", "serviceName": "A-test-fc", "functionName": "test", "Value": 1.0
+  }, { "timestamp": 1602210120000, "userId": "1899690531354629", "region": "cn-shenzhen", "serviceName": "A-test-fc", "functionName": "s", "Value": 1.0 }, { "timestamp": 1602210300000, "userId": "1899690531354629", "region": "cn-shenzhen", "serviceName": "A-test-fc", "functionName": "s", "Value": 2.0 }, { "timestamp": 1602210360000, "userId": "1899690531354629", "region": "cn-shenzhen", "serviceName": "A-test-fc", "functionName": "s", "Value": 1.0 }, { "timestamp": 1602210960000, "userId": "1899690531354629", "region": "cn-shenzhen", "serviceName": "A-test-fc", "functionName": "s", "Value": 1.0 }],
+  "FunctionClientErrors": [
+    {
+      "timestamp": 1602209340000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602209460000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602210120000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210300000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210360000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210780000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602210960000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602212100000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 1
+    },
+    {
+      "timestamp": 1602212400000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "test",
+      "functionName": "fun-nas-function",
+      "Value": 1
+    }
+  ],
+  "FunctionOnDemandUsage": [{"timestamp":1602209340000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"test","Value":128.0},{"timestamp":1602210120000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","Value":128.0},{"timestamp":1602210300000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","Value":256.0},{"timestamp":1602210360000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","Value":128.0},{"timestamp":1602210960000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","Value":128.0}],
+  "FunctionServerErrors": [
+    {
+      "timestamp": 1602209340000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602209460000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210120000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210300000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210360000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210780000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210960000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602212100000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602212400000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "test",
+      "functionName": "fun-nas-function",
+      "Value": 0
+    }
+  ],
+  "FunctionFunctionErrors": [
+    {
+      "timestamp": 1602209340000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602209460000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210120000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210300000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210360000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210780000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602210960000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602212100000,
+      "userId": "1899690531354629",
+      "region": "cn-shanghai",
+      "serviceName": "_FUN_NAS_puppeteer",
+      "functionName": "test",
+      "Value": 0
+    },
+    {
+      "timestamp": 1602212400000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "test",
+      "functionName": "fun-nas-function",
+      "Value": 0
+    }
+  ],
+  "FunctionMaxMemoryUsage": [
+    {
+      "timestamp": 1602209340000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "test",
+      "Value": 18.57
+    },
+    {
+      "timestamp": 1602210120000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 14.2
+    },
+    {
+      "timestamp": 1602210300000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 13.47
+    },
+    {
+      "timestamp": 1602210360000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 13.31
+    },
+    {
+      "timestamp": 1602210960000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 14.28
+    }
+  ],
+  "FunctionAvgDuration": [
+    {
+      "timestamp": 1602209340000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "test",
+      "Value": 54.23
+    },
+    {
+      "timestamp": 1602210120000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 16.77
+    },
+    {
+      "timestamp": 1602210300000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 10.45
+    },
+    {
+      "timestamp": 1602210360000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 4.23
+    },
+    {
+      "timestamp": 1602210960000,
+      "userId": "1899690531354629",
+      "region": "cn-shenzhen",
+      "serviceName": "A-test-fc",
+      "functionName": "s",
+      "Value": 33.06
+    }
+  ],
+  // "FunctionQualifierAsyncEventExpiredDropped": ,
+  "FunctionQualifierDestinationErrors": [{"timestamp":1602209340000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"test","qualifier":"2","__COUNT__":1.0,"destinationErrors":1.0},{"timestamp":1602209460000,"userId":"1899690531354629","region":"cn-shanghai","serviceName":"_FUN_NAS_puppeteer","functionName":"test","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0},{"timestamp":1602210120000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0},{"timestamp":1602210300000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":2.0,"destinationErrors":0.0},{"timestamp":1602210360000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0},{"timestamp":1602210780000,"userId":"1899690531354629","region":"cn-shanghai","serviceName":"_FUN_NAS_puppeteer","functionName":"test","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0},{"timestamp":1602210960000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0},{"timestamp":1602212100000,"userId":"1899690531354629","region":"cn-shanghai","serviceName":"_FUN_NAS_puppeteer","functionName":"test","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0},{"timestamp":1602212400000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"test","functionName":"fun-nas-function","qualifier":"LATEST","__COUNT__":1.0,"destinationErrors":0.0}],
+  "FunctionQualifierDestinationSuccessed": [{"timestamp":1602209340000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"test","qualifier":"2","__COUNT__":1.0,"destinationSucceeded":0.0},{"timestamp":1602209460000,"userId":"1899690531354629","region":"cn-shanghai","serviceName":"_FUN_NAS_puppeteer","functionName":"test","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":3.0},{"timestamp":1602210120000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":0.0},{"timestamp":1602210300000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":2.0,"destinationSucceeded":0.0},{"timestamp":1602210360000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":0.0},{"timestamp":1602210780000,"userId":"1899690531354629","region":"cn-shanghai","serviceName":"_FUN_NAS_puppeteer","functionName":"test","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":0.0},{"timestamp":1602210960000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"A-test-fc","functionName":"s","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":0.0},{"timestamp":1602212100000,"userId":"1899690531354629","region":"cn-shanghai","serviceName":"_FUN_NAS_puppeteer","functionName":"test","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":0.0},{"timestamp":1602212400000,"userId":"1899690531354629","region":"cn-shenzhen","serviceName":"test","functionName":"fun-nas-function","qualifier":"LATEST","__COUNT__":1.0,"destinationSucceeded":0.0}]
+}
